646b91a8261c31bccb805a2001400b21339450b7
Darshan Gulur Srinivasa <Darshan.Srinivasa.contractor@hilton.com>
2022-07-29 16:24:25 -0700

    Added new datastructures

--- LetsPlay/MinStack.playground/Contents.swift
+++ LetsPlay/MinStack.playground/Contents.swift
@@ -1,62 +1,43 @@
 class MinStack {
+    var values = [Int]()
+    var minValues = [Int]()
     
-    struct MinimumInt {
-        let value: Int
-        let minimum: Int
-    }
-
-    var minimum = Int.max
-    var items: [MinimumInt]
     init() {
-        items = [MinimumInt]()
-    }
-    
-    func push(_ x: Int) {
-        let minimum = getMin()
-        if x < minimum {
-            items.append(MinStack.MinimumInt(value: x, minimum: x))
-        } else {
-            items.append(MinStack.MinimumInt(value: x, minimum: minimum))
-        }
-    }
-    
-    func pop() {
-        items.removeLast()
         
-        if items.isEmpty {
-            minimum = Int.max
+    }
+    
+    func push(_ val: Int) {
+        values.append(val)
+        
+        if !minValues.isEmpty, let top = minValues.last {
+            if val > top {
+                minValues.append(top)
+                return
+            }
         }
+        
+        minValues.append(val)
+    }
+    
+    func pop() -> Int {
+        minValues.removeLast()
+        return values.removeLast()
     }
     
     func top() -> Int {
-        return items.last?.value ?? Int.max
+        values.last!
     }
     
     func getMin() -> Int {
-        return items.last?.minimum ?? Int.max
+        minValues.last!
     }
 }
 
-
-// ["MinStack","push","push","push","top","pop","getMin","pop","getMin","pop","push","top","getMin","push","top","getMin","pop","getMin"]
-// [[],[2147483646],[2147483646],[2147483647],[],[],[],[],[],[],[2147483647],[],[],[-2147483648],[],[],[],[]]
-
-let stack = MinStack()
-stack.push(-10)
-stack.push(14)
-stack.getMin()
-stack.getMin()
-stack.push(-20)
-stack.getMin()
-stack.getMin()
-stack.top()
-stack.getMin()
-stack.pop()
-stack.push(10)
-stack.push(-7)
-stack.getMin()
-stack.push(-7)
-stack.pop()
-stack.top()
-stack.getMin()
-stack.pop()
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * let obj = MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * let ret_3: Int = obj.top()
+ * let ret_4: Int = obj.getMin()
+ */
